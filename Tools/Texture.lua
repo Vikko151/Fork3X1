7eb8f706908ff814638007d19ec9bf82c835d7f8
VikkoMakesStuff <208698158+VikkoMakesStuff@users.noreply.github.com>
2025-12-27 13:32:22 +0100

    Update Texture.lua to v3.2.0

--- Tools/Texture.lua
+++ Tools/Texture.lua
@@ -1,20 +1,26 @@
 Tool = script.Parent.Parent;
 Core = require(Tool.Core);
 local Vendor = Tool:WaitForChild('Vendor')
-local UI = Tool:WaitForChild('UI')
-local Libraries = Tool:WaitForChild('Libraries')
+local UI = Core.UIFolder
+local Libraries = Core.Libraries
+local Sounds = Tool:WaitForChild("Sounds");
 
 -- Libraries
 local ListenForManualWindowTrigger = require(Tool.Core:WaitForChild('ListenForManualWindowTrigger'))
 local Roact = require(Vendor:WaitForChild('Roact'))
-local Dropdown = require(UI:WaitForChild('Dropdown'))
 local Signal = require(Libraries:WaitForChild('Signal'))
+local ColorPicker = require(UI:WaitForChild('ColorPicker'))
+local BoundingBox = require(Tool.Core.BoundingBox)
 
 -- Import relevant references
 Selection = Core.Selection;
 Support = Core.Support;
 Security = Core.Security;
-Support.ImportServices();
+
+local TextService = game:GetService("TextService")
+local TextSizeParams = Instance.new("GetTextBoundsParams")
+
+local MinimalTextBoxSize
 
 -- Initialize the tool
 local TextureTool = {
@@ -24,17 +30,35 @@ local TextureTool = {
 	-- Default options
 	Type = 'Decal';
 	Face = Enum.NormalId.Front;
+	ThumbnailMode = "None";
 
 	-- Signals
 	OnFaceChanged = Signal.new();
+	ThumbnailChanged = Signal.new();
 }
 
-TextureTool.ManualText = [[<font face="GothamBlack" size="16">Texture Tool  🛠</font>
-Lets you add decals and textures to parts.<font size="6"><br /></font>
+local ThumbnailModeToSize = {
+	["Asset"] = "w=420&h=420",
+	["Avatar"] = "w=720&h=720",
+	["AvatarHeadShot"] = "w=150&h=150",
+	["BadgeIcon"] = "w=150&h=150",
+	["BundleThumbnail"] = "w=420&h=420",
+	["GameIcon"] = "w=150&h=150",
+	["GamePass"] = "w=150&h=150",
+	["GroupIcon"] = "w=420&h=420",
+	["Outfit"] = "w=420&h=420"
+}
+
+TextureTool.ManualText = [[<font weight="900" size="24"><u><i>Texture Tool  🛠</i></u></font>
+Lets you add decals and textures to parts. Decals are one image fitting the whole surface, whereas textures are repeated images.<font size="6"><br /></font>
+
+<b>TIP: </b>IDs are codes you can find on Roblox's Creator Marketplace or with the marketplace tool<font size="6"><br /></font>
 
 <b>TIP: </b>Click on any part's surface to quickly change a decal/texture's side.<font size="6"><br /></font>
 
 <b>TIP: </b>You can paste the link to any decal and it'll automatically get the right image ID.<font size="6"><br /></font>
+
+<b>TIP: </b>Fast mode uses rbxthumb to load images without experiencing F3X servers slowdowns. However, the quality of the image is twice lower.<font size="6"><br /></font>
 
 <b>NOTE: </b>If HttpService isn't enabled, you must manually type an image's ID.]]
 
@@ -47,6 +71,18 @@ function TextureTool:Equip()
 	-- Start up our interface
 	self:ShowUI()
 	self:EnableSurfaceClickSelection()
+	
+	if Selection.DisableHighlights then
+		BoundingBox.StartBoundingBox(function () end)
+	end
+
+	Connections.BoundingBox = Selection.Changed:Connect(function()
+		if Selection.DisableHighlights and not BoundingBox.GetBoundingBox() then
+			BoundingBox.StartBoundingBox(function () end)
+		elseif not Selection.DisableHighlights and BoundingBox.GetBoundingBox() then
+			BoundingBox.ClearBoundingBox()
+		end
+	end)
 
 	-- Set our current texture type and face
 	self:SetTextureType(self.Type)
@@ -60,6 +96,7 @@ function TextureTool:Unequip()
 	-- Clear unnecessary resources
 	self:HideUI()
 	ClearConnections();
+	BoundingBox.ClearBoundingBox();
 
 end;
 
@@ -74,10 +111,14 @@ function ClearConnections()
 end;
 
 function TextureTool:ShowUI()
+	UI = Core.UIFolder
+	ColorPicker = require(UI:WaitForChild('ColorPicker'))
+	
+	local Dropdown = require(UI:WaitForChild('Dropdown'))
 	-- Creates and reveals the UI
 
 	-- Reveal UI if already created
-	if self.UI then
+	if self.UI and self.UI.Parent ~= nil then
 
 		-- Reveal the UI
 		self.UI.Visible = true
@@ -91,22 +132,44 @@ function TextureTool:ShowUI()
 		return;
 
 	end;
+	
+	if self.UI then
+		self.UI:Destroy()
+	end
 
 	-- Create the UI
-	self.UI = Core.Tool.Interfaces.BTTextureToolGUI:Clone()
+	self.UI = Core.Interfaces.BTTextureToolGUI:Clone()
 	self.UI.Parent = Core.UI
 	self.UI.Visible = true
+	
+	-- Remove any deprecated option if using an older version of the UI
+	if self.UI:FindFirstChild("FastLoadOption") then
+		self.UI.FastLoadOption:Destroy()
+	end
+	if self.UI.ImageIDOption.TextInput:FindFirstChild("BoundingBox") then
+		self.UI.ImageIDOption.TextInput.BoundingBox.TextBox.Parent = self.UI.ImageIDOption.TextInput
+		self.UI.ImageIDOption.TextInput.BoundingBox:Destroy()
+	end
 
 	-- References to UI elements
 	local AddButton = self.UI.AddButton
 	local RemoveButton = self.UI.RemoveButton
 	local DecalModeButton = self.UI.ModeOption.Decal.Button
 	local TextureModeButton = self.UI.ModeOption.Texture.Button
-	local ImageIdInput = self.UI.ImageIDOption.TextBox
+	local ImageIdInput = self.UI.ImageIDOption.TextInput.TextBox
 	local TransparencyInput = self.UI.TransparencyOption.Input.TextBox
 	local RepeatXInput = self.UI.RepeatOption.XInput.TextBox
 	local RepeatYInput = self.UI.RepeatOption.YInput.TextBox
+	local ColorButton = self.UI.ColorOption.HSVPicker
+	local ColorIndicator = self.UI.ColorOption.Indicator
+	
+	MinimalTextBoxSize = ImageIdInput.AbsoluteSize
 
+	TextSizeParams.Font = ImageIdInput:GetStyled("FontFace")
+	TextSizeParams.Size = ImageIdInput:GetStyled("TextSize")
+	TextSizeParams.Width = math.huge
+	TextSizeParams.RichText = false
+	
 	-- Enable the texture type switch
 	DecalModeButton.MouseButton1Click:Connect(function ()
 		self:SetTextureType('Decal')
@@ -124,28 +187,51 @@ function TextureTool:ShowUI()
 		'Left';
 		'Right'
 	};
-	local function BuildFaceDropdown()
+	
+	local Thumbnails = {
+		'None';
+		'Asset';
+		'Avatar';
+		'AvatarHeadShot';
+		'BadgeIcon';
+		'BundleThumbnail';
+		'GameIcon';
+		'GamePass';
+		'GroupIcon';
+		'Outfit'
+	};
+	
+	local function BuildFaceDropdown(OptionsTable, X, CurrentOption, OptionSelected)
 		return Roact.createElement(Dropdown, {
-			Position = UDim2.new(0, 30, 0, 0);
-			Size = UDim2.new(1, -45, 0, 25);
-			Options = Faces;
+			Position = UDim2.new(0, X, 0, 0);
+			Size = UDim2.new(1, -15 - X, 0, 25);
+			Options = OptionsTable;
 			MaxRows = 6;
-			CurrentOption = self.Face and self.Face.Name;
-			OnOptionSelected = function (Option)
-				self:SetFace(Enum.NormalId[Option])
-			end;
+			CurrentOption = CurrentOption;
+			OnOptionSelected = OptionSelected;
 		})
 	end
 
 	-- Mount type dropdown
-	local FaceDropdownHandle = Roact.mount(BuildFaceDropdown(), self.UI.SideOption, 'Dropdown')
-	self.OnFaceChanged:Connect(function ()
-		Roact.update(FaceDropdownHandle, BuildFaceDropdown())
+	local FaceDropdownHandle = Roact.mount(BuildFaceDropdown(Faces, 30, self.Face.Name, function(Option) self:SetFace(Enum.NormalId[Option]) end), self.UI.SideOption, 'Dropdown')
+	self.OnFaceChanged:Connect(function (Option)
+		Roact.update(FaceDropdownHandle, BuildFaceDropdown(Faces, 30, Option.Name, function(Option) self:SetFace(Enum.NormalId[Option]) end))
 	end)
+	local ThumbnailDropdownHandle = Roact.mount(BuildFaceDropdown(Thumbnails, 110, self.ThumbnailMode, function(Option) self.ThumbnailChanged:Fire(Option) end), self.UI.ThumbnailOption, 'Dropdown')
+	self.ThumbnailChanged:Connect(function (Option)
+		self.ThumbnailMode = Option
+		Roact.update(ThumbnailDropdownHandle, BuildFaceDropdown(Thumbnails, 110, self.ThumbnailMode, function(Option) self.ThumbnailChanged:Fire(Option) end))
+	end)
+	
+--	FastLoadToggle.Activated:Connect(function()
+--		self.FastLoad = not self.FastLoad
+--		game:GetService("SoundService"):PlayLocalSound(Sounds:WaitForChild("Press"))
+--		UpdateToggleInput(FastLoadToggle, self.FastLoad)
+--	end)
 
 	-- Enable the image ID input
 	ImageIdInput.FocusLost:Connect(function (EnterPressed)
-		SetTextureId(TextureTool.Type, TextureTool.Face, ParseAssetId(ImageIdInput.Text));
+		SetTextureId(TextureTool.Type, TextureTool.Face, tonumber(ParseAssetId(ImageIdInput.Text)) or ImageIdInput.Text);
 	end);
 
 	-- Enable other inputs
@@ -155,15 +241,46 @@ function TextureTool:ShowUI()
 
 	-- Enable the texture adding button
 	AddButton.Button.MouseButton1Click:Connect(function ()
+		game:GetService("SoundService"):PlayLocalSound(Sounds:WaitForChild("Add"))
 		AddTextures(TextureTool.Type, TextureTool.Face);
 	end);
+	AddButton.Button.MouseEnter:Connect(function ()
+		game:GetService("SoundService"):PlayLocalSound(Sounds:WaitForChild("Hover"))
+	end);
 	RemoveButton.Button.MouseButton1Click:Connect(function ()
+		game:GetService("SoundService"):PlayLocalSound(Sounds:WaitForChild("Remove"))
 		RemoveTextures(TextureTool.Type, TextureTool.Face);
 	end);
+	RemoveButton.Button.MouseEnter:Connect(function ()
+		game:GetService("SoundService"):PlayLocalSound(Sounds:WaitForChild("Hover"))
+	end);
+	
+	local ColorPickerHandle = nil
+	ColorButton.MouseButton1Click:Connect(function ()
+		local CommonColor = Support.IdentifyCommonProperty(GetTextures(TextureTool.Type, TextureTool.Face), "Color3")
+		local ColorPickerElement = Roact.createElement(ColorPicker, {
+			InitialColor = CommonColor or Color3.fromRGB(255, 255, 255);
+			SetPreviewColor = function (Color)
+				SetPreviewColor(TextureTool.Type, "Color3", Color)
+			end;
+			OnConfirm = function (Color)
+				SetProperty(TextureTool.Type, TextureTool.Face, "Color3", Color)
+				ColorPickerHandle = Roact.unmount(ColorPickerHandle)
+			end;
+			OnCancel = function ()
+				ColorPickerHandle = Roact.unmount(ColorPickerHandle)
+			end;
+		})
+		ColorPickerHandle = ColorPickerHandle and
+			Roact.update(ColorPickerHandle, ColorPickerElement) or
+			Roact.mount(ColorPickerElement, Core.UI, 'ColorPicker')
+	end)
 
 	-- Hook up manual triggering
 	local SignatureButton = self.UI:WaitForChild('Title'):WaitForChild('Signature')
 	ListenForManualWindowTrigger(TextureTool.ManualText, TextureTool.Color.Color, SignatureButton)
+	
+	TextureTool:ChangeLayout(Layouts.EmptySelection)
 
 	-- Update the UI every 0.1 seconds
 	self.StopUpdatingUI = Support.Loop(0.1, function ()
@@ -198,6 +315,56 @@ function TextureTool:EnableSurfaceClickSelection()
 			self:SetFace(Core.Mouse.TargetSurface)
 		end
 	end)
+
+end;
+
+local PreviewInitialState = nil
+
+function SetPreviewColor(TextType, Property, Color)
+
+	-- Reset colors to initial state if previewing is over
+	if not Color and PreviewInitialState then
+		for Text, State in pairs(PreviewInitialState) do
+			Text[Property] = State[Property]
+		end
+
+		-- Clear initial state
+		PreviewInitialState = nil
+
+		-- Skip rest of function
+		return
+
+			-- Ensure valid color is given
+	elseif not Color then
+		return
+
+			-- Save initial state if first time previewing
+	elseif not PreviewInitialState then
+		PreviewInitialState = {}
+		for _, Text in pairs(GetTextures(TextType, TextureTool.Face)) do
+			PreviewInitialState[Text] = { [Property] = Text[Property] }
+		end
+	end
+
+	-- Apply preview color
+	for Text in pairs(PreviewInitialState) do
+		Text[Property] = Color
+	end
+end
+
+function UpdateColorIndicator(Indicator, Color)
+	-- Updates the given color indicator
+
+	-- If there is a single color, just display it
+	if Color then
+		Indicator.BackgroundColor3 = Color;
+		Indicator.Varies.Text = '';
+
+		-- If the colors vary, display a * on a gray background
+	else
+		Indicator.BackgroundColor3 = Color3.new(222/255, 222/255, 222/255);
+		Indicator.Varies.Text = '*';
+	end;
 
 end;
 
@@ -243,17 +410,17 @@ end;
 local TextureTypes = { 'Decal', 'Texture' };
 
 -- List of UI layouts
-local Layouts = {
+Layouts = {
 	EmptySelection = { 'SelectNote' };
-	NoTextures = { 'ModeOption', 'SideOption', 'AddButton' };
-	SomeDecals = { 'ModeOption', 'SideOption', 'ImageIDOption', 'TransparencyOption', 'AddButton', 'RemoveButton' };
-	AllDecals = { 'ModeOption', 'SideOption', 'ImageIDOption', 'TransparencyOption', 'RemoveButton' };
-	SomeTextures = { 'ModeOption', 'SideOption', 'ImageIDOption', 'TransparencyOption', 'RepeatOption', 'AddButton', 'RemoveButton' };
-	AllTextures = { 'ModeOption', 'SideOption', 'ImageIDOption', 'TransparencyOption', 'RepeatOption', 'RemoveButton' };
+	NoTextures = { 'ModeOption', 'SideOption', 'ThumbnailOption', 'AddButton' };
+	SomeDecals = { 'ModeOption', 'SideOption', 'ThumbnailOption', 'ImageIDOption', 'TransparencyOption', 'ColorOption', 'AddButton',  'RemoveButton' };
+	AllDecals = { 'ModeOption', 'SideOption', 'ThumbnailOption', 'ImageIDOption', 'TransparencyOption', 'ColorOption', 'RemoveButton'};
+	SomeTextures = { 'ModeOption', 'SideOption', 'ThumbnailOption', 'ImageIDOption', 'TransparencyOption', 'RepeatOption', 'ColorOption', 'AddButton', 'RemoveButton' };
+	AllTextures = { 'ModeOption', 'SideOption', 'ThumbnailOption', 'ImageIDOption', 'TransparencyOption', 'RepeatOption', 'ColorOption', 'RemoveButton' };
 };
 
 -- List of UI elements
-local UIElements = { 'SelectNote', 'ModeOption', 'SideOption', 'ImageIDOption', 'TransparencyOption', 'RepeatOption', 'AddButton', 'RemoveButton' };
+local UIElements = { 'SelectNote', 'ColorOption', 'ThumbnailOption', 'ModeOption', 'SideOption', 'ImageIDOption', 'TransparencyOption', 'RepeatOption', 'AddButton', 'RemoveButton' };
 
 -- Current UI layout
 local CurrentLayout;
@@ -300,9 +467,11 @@ function TextureTool:ChangeLayout(Layout)
 	end;
 
 	-- Resize the container to fit the new layout
-	self.UI.Size = UDim2.new(0, 200, 0, 30 + Sum)
+	self.UI.Size = UDim2.new(0, 240, 0, 30 + Sum)
 
 end;
+
+local LastText = ""
 
 function TextureTool:UpdateUI()
 	-- Updates information on the UI
@@ -316,8 +485,9 @@ function TextureTool:UpdateUI()
 	local Textures = GetTextures(TextureTool.Type, TextureTool.Face);
 
 	-- References to UI elements
-	local ImageIdInput = self.UI.ImageIDOption.TextBox;
+	local ImageIdInput = self.UI.ImageIDOption.TextInput.TextBox;
 	local TransparencyInput = self.UI.TransparencyOption.Input.TextBox;
+	local ColorIndicator = self.UI.ColorOption.Indicator;
 
 	-----------------------
 	-- Update the UI layout
@@ -352,6 +522,7 @@ function TextureTool:UpdateUI()
 	-- Get the common properties
 	local ImageId = Support.IdentifyCommonProperty(Textures, 'Texture');
 	local Transparency = Support.IdentifyCommonProperty(Textures, 'Transparency');
+	local Color = Support.IdentifyCommonProperty(Textures, 'Color3');
 
 	-- Update the common inputs
 	UpdateDataInputs {
@@ -371,12 +542,26 @@ function TextureTool:UpdateUI()
 		local RepeatY = Support.IdentifyCommonProperty(Textures, 'StudsPerTileV');
 
 		-- Update inputs
+		UpdateColorIndicator(ColorIndicator, Support.IdentifyCommonProperty(Textures, 'Color3'));
 		UpdateDataInputs {
 			[RepeatXInput] = RepeatX and Support.Round(RepeatX, 3) or '*';
 			[RepeatYInput] = RepeatY and Support.Round(RepeatY, 3) or '*';
 		};
 
 	end;
+	
+	TextSizeParams.Text = ImageIdInput.Text
+
+	--BoundingBox.Text = TextInput.Text
+	local Size = TextService:GetTextBoundsAsync(TextSizeParams)
+
+	ImageIdInput.Size = UDim2.new(0, math.max(Size.X, MinimalTextBoxSize.X), 1, -ImageIdInput.Parent.ScrollBarThickness) 
+
+	if ImageIdInput.Text ~= LastText and ImageIdInput:IsFocused() then
+		ImageIdInput.Parent.CanvasPosition = Vector2.new(ImageIdInput.Parent.AbsoluteCanvasSize.X - MinimalTextBoxSize.X, 0)
+	end
+
+	LastText = ImageIdInput.Text
 
 end;
 
@@ -398,6 +583,23 @@ function UpdateDataInputs(Data)
 
 end;
 
+function UpdateToggleInput(Toggle, Data)
+	-- Updates the data in the given buttons
+
+	-- Go through the inputs and data
+	if Data == true then
+		Toggle.Mark.Visible = true
+		Toggle.Multiple.Visible = false
+	elseif Data == false then
+		Toggle.Mark.Visible = false
+		Toggle.Multiple.Visible = false
+	else
+		Toggle.Mark.Visible = false
+		Toggle.Multiple.Visible = true
+	end
+
+end;
+
 function ParseAssetId(Input)
 	-- Returns the intended asset ID for the given input
 
@@ -405,7 +607,7 @@ function ParseAssetId(Input)
 	local Id = tonumber(Input)
 		or tonumber(Input:lower():match('%?id=([0-9]+)'))
 		or tonumber(Input:match('/([0-9]+)/'))
-		or tonumber(Input:lower():match('rbxassetid://([0-9]+)'));
+		or tonumber(Input:lower():match('rbxassetid://([0-9]+)'))
 
 	-- Return the ID
 	return Id;
@@ -423,8 +625,12 @@ function TextureTool:SetTextureType(TextureType)
 
 	-- Update the UI
 	Core.ToggleSwitch(TextureType, self.UI.ModeOption);
+	if self.UI.AddButton.Button:IsA("TextButton") then
 	self.UI.AddButton.Button.Text = 'ADD ' .. TextureType:upper();
+	end
+	if self.UI.RemoveButton.Button:IsA("TextButton") then
 	self.UI.RemoveButton.Button.Text = 'REMOVE ' .. TextureType:upper();
+	end
 
 end;
 
@@ -461,17 +667,33 @@ function SetTextureId(TextureType, Face, AssetId)
 	if not AssetId then
 		return;
 	end;
-
+	
+	local Changes
+	local SearchForImage = false
+	
 	-- Prepare the change request
-	local Changes = {
-		Texture = 'rbxassetid://' .. AssetId;
-	};
+	if tonumber(AssetId) == nil then
+		Changes = {
+			Texture = AssetId;
+		};
+	elseif TextureTool.ThumbnailMode ~= "None" then
+		Changes = {
+			Texture = string.format("rbxthumb://type=%s&id=%d&%s", TextureTool.ThumbnailMode, AssetId, ThumbnailModeToSize[TextureTool.ThumbnailMode]);
+		};
+	else
+		SearchForImage = true
+		Changes = {
+			Texture = 'rbxassetid://' .. AssetId;
+		};
+	end
 
 	-- Attempt an image extraction on the given asset
+	if SearchForImage == true then
 	Core.Try(Core.SyncAPI.Invoke, Core.SyncAPI, 'ExtractImageFromDecal', AssetId)
 		:Then(function (ExtractedImage)
 			Changes.Texture = 'rbxassetid://' .. ExtractedImage;
 		end);
+	end
 
 	-- Start a history record
 	TrackChange();
