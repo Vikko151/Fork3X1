9d9c75cf62fccb1d1edab97d9dfb7ae17faaff22
VikkoMakesStuff <208698158+VikkoMakesStuff@users.noreply.github.com>
2025-12-31 14:17:55 +0100

    Update AltTooltip.lua to v3.2.0

--- UI/ScopeHUD/HotkeyTooltip/AltTooltip.lua
+++ UI/ScopeHUD/HotkeyTooltip/AltTooltip.lua
@@ -4,7 +4,13 @@ local TextService = game:GetService('TextService')
 
 -- Libraries
 local Roact = require(Vendor:WaitForChild 'Roact')
-local new = Roact.createElement
+local new = function(Type, props, ...)
+	if props["RBXTAG_Native"] == nil then
+		props["RBXTAG_Native"] = true
+	end
+
+	return Roact.createElement(Type, props, ...)
+end
 
 -- Calculate label length
 local LABEL_TEXT = 'ALT'
@@ -13,27 +19,27 @@ local LABEL_LENGTH = TextService:GetTextSize(LABEL_TEXT,
 
 local function ScopeInTooltip(props)
     return new('ImageLabel', {
-        BackgroundTransparency = 1;
-        ImageTransparency = 1 - 0.14;
-        Image = 'rbxassetid://4445959523';
-        ScaleType = Enum.ScaleType.Slice;
-        SliceCenter = Rect.new(4, 4, 12, 12);
-        ImageColor3 = Color3.fromRGB(0, 0, 0);
+    --    BackgroundTransparency = 1;
+    --    ImageTransparency = 1 - 0.14;
+    --    Image = 'rbxassetid://4445959523';
+     --   ScaleType = Enum.ScaleType.Slice;
+    --    SliceCenter = Rect.new(4, 4, 12, 12);
+     --   ImageColor3 = Color3.fromRGB(0, 0, 0);
         Size = UDim2.new(0, LABEL_LENGTH + 8/2, 0, 30/2);
-        LayoutOrder = props.LayoutOrder or 3;
+		LayoutOrder = props.LayoutOrder or 3;
     },
     {
         Label = new('TextLabel', {
-            BackgroundTransparency = 1;
-            Size = UDim2.new(0, LABEL_LENGTH, 1, 0);
-            Position = UDim2.new(0, 5/2, 0.5, 0);
-            AnchorPoint = Vector2.new(0, 0.5);
-            Font = Enum.Font.SourceSansSemibold;
-            TextSize = 21/2;
+        --    BackgroundTransparency = 1;
+        --    Size = UDim2.new(0, LABEL_LENGTH, 1, 0);
+        --    Position = UDim2.new(0, 5/2, 0.5, 0);
+        --    AnchorPoint = Vector2.new(0, 0.5);
+        --    Font = Enum.Font.SourceSansSemibold;
+        --    TextSize = 21/2;
             Text = LABEL_TEXT;
-            TextTransparency = 0.5;
-            TextYAlignment = Enum.TextYAlignment.Center;
-            TextColor3 = Color3.fromRGB(255, 255, 255);
+        --    TextTransparency = 0.5;
+        --    TextYAlignment = Enum.TextYAlignment.Center;
+        --    TextColor3 = Color3.fromRGB(255, 255, 255);
             LayoutOrder = 1;
         });
     })
